c149914bbfb0bd7d5330fc5e4e71d404e794b76c
chyroc <chyroc@qq.com>
2023-02-18 08:11:47 +0800

    Add support for printing IPv4/IPv6 ping in json format

--- cli/detect-ip/main.go
+++ cli/detect-ip/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"time"
@@ -10,16 +11,54 @@ import (
 
 func main() {
 	i := flag.Int64("timeout", 5000, "timeout(ms)")
+	isIPv4 := flag.Bool("v4", true, "print ipv4")
+	isIPv6 := flag.Bool("v6", true, "print ipv6")
+	isJson := flag.Bool("json", false, "print json")
 	flag.Parse()
 
-	timeout := time.Duration(*i) * time.Millisecond
+	result := detectIP(*i, *isIPv4, *isIPv6)
+
+	result.Print(*isJson)
+}
+
+type Result struct {
+	IPv4     string `json:"ipv4,omitempty"`
+	IPv4Ping string `json:"ipv4_ping,omitempty"`
+	IPv6     string `json:"ipv6,omitempty"`
+	IPv6Ping string `json:"ipv6_ping,omitempty"`
+}
+
+func detectIP(i int64, isIPv4, isIPv6 bool) *Result {
+	timeout := time.Duration(i) * time.Millisecond
+	result := new(Result)
 
 	start := time.Now()
-	ipv4 := detect_ip.PublicIPV4(detect_ip.WithTimeout(timeout))
-	ipv4Ping := time.Since(start)
-	ipv6 := detect_ip.PublicIPV6(detect_ip.WithTimeout(timeout))
-	ipv6Ping := time.Since(start) - ipv4Ping
+	ipv4Ping := time.Duration(0)
+	if isIPv4 {
+		ipv4 := detect_ip.PublicIPV4(detect_ip.WithTimeout(timeout))
+		result.IPv4 = ipv4.String()
+		ipv4Ping = time.Since(start)
+		result.IPv4Ping = ipv4Ping.String()
+	}
+	if isIPv6 {
+		ipv6 := detect_ip.PublicIPV6(detect_ip.WithTimeout(timeout))
+		result.IPv6 = ipv6.String()
+		result.IPv6Ping = (time.Since(start) - ipv4Ping).String()
+	}
 
-	fmt.Printf("ipv4: %s, ping: %s\n", ipv4.String(), ipv4Ping.String())
-	fmt.Printf("ipv6: %s, ping: %s\n", ipv6.String(), ipv6Ping.String())
+	return result
+}
+
+func (r Result) Print(isJSON bool) {
+	if isJSON {
+		bs, _ := json.MarshalIndent(r, "", "  ")
+		fmt.Println(string(bs))
+	} else {
+		if r.IPv4 != "" {
+			fmt.Printf("ipv4: %s, ping: %s\n", r.IPv4, r.IPv4Ping)
+		}
+		if r.IPv6 != "" {
+			fmt.Printf("ipv6: %s, ping: %s\n", r.IPv6, r.IPv6Ping)
+		}
+	}
 }
